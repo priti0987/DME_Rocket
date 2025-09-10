d739bdbfc5405bf8edaaa77a8e9fa14b05556ec7
Arun hashroot <arunrx2484@gmail.com>
2025-09-10 19:52:09 +0530

    chore: update navigation steps

--- src/steps/navigation.steps.cjs
+++ src/steps/navigation.steps.cjs
@@ -1,4 +1,15 @@
-const { Given, When, Then } = require(@cucumber/cucumber);
-const { expect } = require(@playwright/test);
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
 
-Given(I
+Given('I open the base url', async function () {
+  await this.page.goto(this.baseUrl, { waitUntil: 'domcontentloaded' });
+});
+
+When('I click the first link on the page', async function () {
+  const firstLink = this.page.locator('a').first();
+  await firstLink.click();
+});
+
+Then('the page URL contains {string}', async function (text) {
+  await expect(this.page).toHaveURL(new RegExp(text, 'i'));
+});
